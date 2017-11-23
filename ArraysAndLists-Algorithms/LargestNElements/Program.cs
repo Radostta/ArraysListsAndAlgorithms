df51494d00d06ba7f3b0a6379727f70f3b71220e
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-23 09:24:13 +0200

    Adding solutions of the lab problems

---
+++ ArraysAndLists-Algorithms/LargestNElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestNElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            var result = new List<int>();
+            numbers.Sort();
+
+            for (int i = numbers.Count - 1; i > numbers.Count - 1 - n; i--)
+            {
+                result.Add(numbers[i]);
+            }
+
+            //numbers.Sort((a, b) => (b - a));
+            //result.AddRange(numbers.Take(n));
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
