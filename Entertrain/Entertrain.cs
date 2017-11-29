5e6438c46aadc168a2d5b7fd80e3d1554931ff47
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-29 23:06:14 +0200

    Adding Entertrain.cs

---
+++ Entertrain/Entertrain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertrain
+{
+    class Entertrain
+    {
+        static void Main(string[] args)
+        {
+            int locomotivePower = int.Parse(Console.ReadLine());
+            var trainSequence = new List<int>();
+
+            double sum = 0;
+            double average = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "All ofboard!")
+            {
+                trainSequence.Add(int.Parse(input));
+                sum = trainSequence.Sum();
+
+                if (sum > locomotivePower)
+                {
+                    average = trainSequence.Average();
+                    double minDifference = average;
+                    int closestElement = 0;
+
+                    for (int i = 0; i < trainSequence.Count; i++)
+                    {
+                        double currentDifference = Math.Abs(average - trainSequence[i]);
+
+                        if (currentDifference < minDifference )
+                        {
+                            minDifference = currentDifference;
+                            closestElement = trainSequence[i];
+                        }                        
+                    }
+
+                    int closestElementIndex = trainSequence.IndexOf(closestElement);
+                    trainSequence.RemoveAt(closestElementIndex);                    
+                }
+
+                input = Console.ReadLine();
+            }
+
+            trainSequence.Reverse();
+            trainSequence.Add(locomotivePower);
+
+            Console.WriteLine(string.Join(" ", trainSequence));
+            
+        }
+    }
+}
